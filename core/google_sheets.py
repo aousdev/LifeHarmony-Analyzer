f9203fe2162f1746c603b72e0572d61055986f10
aousdev <askriahlem100@gmail.com>
2023-10-20 22:49:54 +0200

    Integrate Google Sheets API for data retrieval: In this commit, I integrated the Google Sheets API to facilitate data retrieval from the 'Wellbeing_and_lifestyle_data_Kaggle' spreadsheet. The following changes were made:
    1. Imported necessary modules, including datetime, re, gspread, and Google OAuth2 Credentials.
    2. Defined the required Google Sheets API scope.
    3. Obtained credentials from a 'creds.json' service account file.
    4. Authorized the Google Sheets API client with the specified scope.
    5. Opened the 'main' worksheet of the 'Wellbeing_and_lifestyle_data_Kaggle' spreadsheet.
    
    These changes enable the program to access and retrieve data from the
    Google Sheets document, which is a crucial part of its functionality.

---
+++ core/google_sheets.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import re
+import gspread
+from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Wellbeing_and_lifestyle_data_Kaggle')
+
+mainWorkSheet = SHEET.worksheet('main')
