00dec9c4114b71893d0f3292f97c37028844b9d6
aousdev <askriahlem100@gmail.com>
2023-10-20 22:47:16 +0200

    Update the main script: - Refactor the main.py file to import the `start` function from the `core.start` module, which follows a modular structure for better code organization. This change enhances maintainability and readability by separating program logic into distinct modules.

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import gspread
-from google.oauth2.service_account import Credentials
+from core.start import start
 
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('Wellbeing_and_lifestyle_data_Kaggle')
-
-mainWorkSheet = SHEET.worksheet('main')
-
-data = mainWorkSheet.get_all_values()
-
-print(data)
+if __name__ == "__main__":
+    start()
